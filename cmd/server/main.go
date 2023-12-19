5fb54a3d9bcfeba9dbee7a8ba805a8877c495bd8
Humberto Neto <humberto.neto@unitri.edu.br>
2023-12-19 14:37:54 -0300

    dto para trafegar dados pela aplicação

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/machadoborges1/go_api/configs"
+	"github.com/machadoborges1/go_api/internal/entity"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBPort)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	http.ListenAndServe(":8000", nil)
 }
